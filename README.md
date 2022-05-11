843beeb530fdab0894f5cca6bd99074c738ba45f
aliveGUY <ilyadubrivvin@gmail.com>
2022-05-11 14:01:39 +0300

    SetUp WebPack

---
+++ README.md
@@ -0,0 +1,8 @@
+# You can refer to this repo if you want to implement some math in your markup
+
+## Curently im working at:
+🪡 Graphing mathematical functions 
+
+## Implemented features:
+
+✔️ N/A
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: 'docs',
+    hot: true,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'docs'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
